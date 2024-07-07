772a68c0635803617068b8d642f890c56f9d4995
jobsong <2115187266@qq.com>
2024-07-07 13:53:55 +0800

    修正首页、登录页面

--- frontend/src/views/Entrance/Entrance.vue
+++ frontend/src/views/Entrance/Entrance.vue
@@ -1,8 +1,13 @@
 <template>
   <!--    入口-->
   <div class="back-entrance">
-    <img  class="logo" src="../../image/logo.png" alt="">
-    <div style="display: flex; flex-direction: column;justify-content: center;">
+
+
+    <div :style="style1" class="Entrancelogo" >
+      <img  style="width:150px;margin-top:14vh;margin-left:-2vh" src="../../image/logo.png" alt=""  >
+    </div>
+
+    <div :style="style2" class="Entrancebigbox" >
       <h1 class="logoName">铜杆车间调度系统平台</h1>
       <h1 class="welcome">欢迎使用铜杆车间调度系统平台</h1>
       <h2 class="target">致力于打造车间流程全可视化、人员调度更便捷的铜杆车间调度系统</h2>
@@ -17,6 +22,14 @@ export default{
   methods:{
     goToLogin(){
       this.$router.push({name:'login'})
+    },
+    style1:{
+      position: 'absolute',
+      zIndex: 2,
+    },
+    style2:{
+      position: 'absolute',
+      zIndex: 1,
     }
   }
 }
@@ -24,60 +37,71 @@ export default{
 </script>
 
 <style>
+
+
 .back-entrance{
-  background-image: url("../../../public/module/backpage.jpg");
+  background-image: url("../../image/backpage.jpg");
   background-size: cover;
   background-position: center;
   height: 100vh;
   width: 100vw;
-  margin: 0 auto;
-  display:flex;
-  justify-content: center;
-  .logo{
-    width: 157px;
-    height: 140px;
-    margin-top:25vh;
-    margin-left:10vw;
-  };
-  .logoName{
-    width: 378px;
-    height: 106px;
-    font-size: 37px;
-    font-weight: bold;
-    line-height: 53px;
-    color: #0F4264;
+
+  .Entrancelogo{
+    width: 100px;
+    height: 100px;
     margin-top:0vh;
+    margin-left:47vw;
+
   };
-  .welcome{
-    width: 672px;
-    height: 67px;
-    font-size: 48px;
-    font-weight: bold;
-    line-height: 67px;
-    letter-spacing: 0;
-    color: #000000;
-    margin-left:-10vw;
-    margin-top:-8vh;
-  };
-  .target{
-    width: 720px;
-    height: 34px;
-    font-size: 24px;
-    font-weight: bold;
-    line-height: 34px;
-    display: flex;
-    align-items: center;
-    letter-spacing: 0;
-    color: #BB4430;
-    margin-left:-12vw;
-    margin-top:-2vh;
-  }
-  #button{
-    width: 300px;
-    height: 49px;
-    background: #4F4F4F;
-    font-size: 25px;
-    color: white;
+
+  .Entrancebigbox{
+    margin-top:20vh;
+    margin-left:42vw;
+    background: rgba(0,0, 0, 0);
+    border: 3px solid rgba(0, 0, 0, 0);
+
+    .logoName{
+      width: 378px;
+      height: 106px;
+      font-size: 37px;
+      font-weight: bold;
+      line-height: 53px;
+      margin-left:-2vw;
+      color: #0F4264;
+      margin-top:0vh;
+    };
+    .welcome{
+      width: 672px;
+      height: 67px;
+      font-size: 48px;
+      font-weight: bold;
+      line-height: 67px;
+      letter-spacing: 0;
+      color: #000000;
+      margin-left:-10vw;
+      margin-top:-8vh;
+    };
+    .target{
+      width: 720px;
+      height: 34px;
+      font-size: 24px;
+      font-weight: bold;
+      line-height: 34px;
+      display: flex;
+      align-items: center;
+      letter-spacing: 0;
+      color: #BB4430;
+      margin-left:-11vw;
+      margin-top:-2vh;
+    }
+
+    #button{
+      width: 300px;
+      height: 49px;
+      background: #4F4F4F;
+      font-size: 25px;
+      color: white;
+    }
   }
 }
 </style>
